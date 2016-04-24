5a61d3042ba9158f753d0eadf62ae403a6dfda0d
max <lejjer@gmail.com>
2016-04-25 00:45:10 +0300

    Task 2 needs check.

--- 1/1.2/Unit1.pas
+++ 1/1.2/Unit1.pas
@@ -5,6 +5,7 @@ uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, TeEngine, Series, ExtCtrls, TeeProcs, Chart, StdCtrls, RK_Method;
 
+  function calcQ(V:Real; h:Real):Real;
   function f1(VarArray:TVarsArray):extended;
   function f2(VarArray:TVarsArray):extended;
   function f3(VarArray:TVarsArray):extended;
@@ -16,7 +17,10 @@ type
     { Private declarations }
   public
     { Public declarations }
+    Memo1 : TMemo;
     procedure calc();
+    procedure print(str:string);
+    
   end;
 
 var
@@ -41,27 +45,70 @@ implementation
 {$R *.dfm}
 
 //Задаем функции (правые части уравнений)
+
+function calcQ(V:Real; h: Real):Real;
+var
+  p : Real;
+begin
+  p := h/100;
+  Result := p*V*V/2;
+end;
+
 function f1(VarArray:TVarsArray):extended;
 var
-  q : Real;
+  q,V,h, phi,L : Real;
 begin
-    q := VarArray[0];
-    Result:= -Cx*q*Sm/m-g*cos(q);
+    V := VarArray[1];
+    phi := VarArray[2];
+    L := VarArray[3];
+    h := VarArray[4];
+    q := calcQ(V,h);
+    Result:= -Cx*q*Sm/m-g*cos(phi);
 end;
 
 function f2(VarArray:TVarsArray):extended;
+var
+  q,V,h, phi,L : Real;
 begin
-   Result:=1;
+    V := VarArray[1];
+    phi := VarArray[2];
+    L := VarArray[3];
+    h := VarArray[4];
+    Form1.print('V='+floattostr(V));
+    Form1.print('phi='+floattostr(phi));
+    Form1.print('L='+floattostr(L));
+    Form1.print('h='+floattostr(h));
+    q := calcQ(V,h);
+  Result:= Cy*q*Sm/m/V - (q/V - V/(R+h))*cos(phi);
 end;
 
 function f3(VarArray:TVarsArray):extended;
+var
+  q,V,h, phi,L : Real;
 begin
-   Result:=1;
+    V := VarArray[1];
+    phi := VarArray[2];
+    L := VarArray[3];
+    h := VarArray[4];
+    q := calcQ(V,h);
+  Result:= R/(R+h)*V*cos(phi);
 end;
 
 function f4(VarArray:TVarsArray):extended;
+var
+  q,V,h, phi,L : Real;
 begin
-   Result:=1;
+    V := VarArray[1];
+    phi := VarArray[2];
+    L := VarArray[3];
+    h := VarArray[4];
+    q := calcQ(V,h);
+  Result:= V*sin(phi);
+end;
+
+procedure TForm1.print(str:string);
+begin
+  Self.Memo1.Lines.Add(str);
 end;
 
 procedure TForm1.calc();
@@ -73,7 +120,7 @@ var
   Steps: Integer; // число шагов по независимой координате
   InitArray: TInitArray; // вектор начальных значений
   Res: TResArray; // матрица результатов включая независ. переменную
-  Memo1 : TMemo;
+  //Memo1 : TMemo;
 begin
   Memo1 := TMemo.Create(Self);
   Memo1.Parent := Self;
@@ -87,19 +134,19 @@ begin
   FunArray[2]:=f3;
   FunArray[3]:=f4;
   // Задаем интервал и число шагов:
-  First:=0;
+  First:=-30;
   Last:=10;
   Steps:=10;
   // Задаем начальные условия:
   SetLength(InitArray,4);
-  InitArray[0]:=0;
-  InitArray[1]:=0;
-  InitArray[2]:=0;
+  InitArray[0]:=50;
+  InitArray[1]:=-25;
+  InitArray[2]:=100;
   InitArray[3]:=0;
   // Вызов метода и получение результатов:
-  Memo1.Lines.Clear;
+  //Memo1.Lines.Clear;
   I:=Runge_Kutt(FunArray, First, Last, Steps, InitArray, Res);
-  //ShowMessage('Код ошибки = '+IntToStr(I));
+  ShowMessage('Код ошибки = '+IntToStr(I));
   For I:=0 to Steps do
   Memo1.Lines.Add(floattostr(Res[0,I])+'   '+floattostr(Res[1,I])
   +'   '+floattostr(Res[2,I]));
